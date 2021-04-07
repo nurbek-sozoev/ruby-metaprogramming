3c775f8262107bd9e26a580b891c9ce901fb51e3
Nurbek Sozoev <sozoev@gmail.com>
2021-04-07 12:49:03 +0600

    Обновил eigenclass

--- src/eigenclass.rb
+++ src/eigenclass.rb
@@ -1,11 +1,37 @@
-john = "John"
-
 # Конструкция `class <<` позволяет получить доступ к метаклассу объекта
-class << john
-  def hello
-    "Hello, #{ self }!"
+
+class SomeClass
+  def hi
+    puts "Hi!"
   end
 end
 
-puts john       # > John
-puts john.hello # > Hello, John!
+# Eigenclass класса SomeClass
+class << SomeClass
+  def hello
+    puts "Hello!"
+  end
+end
+
+class OtherClass < SomeClass; end
+
+other_var = OtherClass.new
+some_var = SomeClass.new
+
+# Eigenclass переменной some_var (instance класса SomeClass)
+class << some_var
+  def bye
+    puts "Bye!"
+  end
+end
+
+some_var.hi    # > Hi!
+some_var.bye   # > Bye!
+# some_var.hello # Ошибка, нет такого метода
+
+other_var.hi    # > Hi!
+# other_var.bye   # Ошибка, нет такого метода
+# other_var.hello # Ошибка, нет такого метода
+
+SomeClass.hello  # > Hello!
+OtherClass.hello # > Hello!
